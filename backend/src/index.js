f535278130ecd2acb41ccaa12c88ec930e60b890
gmiserani <gabrielamiserani@uol.com.br>
2022-09-19 10:04:24 -0300

    configuracao de ambiente do backend

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express'); //chama a biblioteca instalada
+require('dotenv').config();
+
+const app = express(); //faz uso da biblioteca instalada em uma constante pra ficar mais facil de chamar o express
+/*global process*/
+const port = process.env.PORT; //caminho para acessar a variavel
+//api tem que rodar em uma porta para que o front possa acessar essa porta
+app.listen(port, console.log(`Server is running on port ${port}`));
+
+const user = [ {
+    id: 1,
+    name: 'user',
+},
+{
+    id:2,
+    name: 'user2',
+}
+
+];
+
+app.get('/', (req,res) => {
+    res.send(user);
+}); //get chama a api -> quando chamar vai responder hello world
